1a33f3ce1f83afb90bc883ed819ce27cfb57adde
hazeelin <hazeelin@yahoo.com>
2021-01-20 18:36:55 +0000

    Add files via upload

---
+++ MyCircularBuffer/CircularBuffer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCircularBuffer
+{
+    public class CircularBuffer<T>
+    {
+        private T[] _buffer;
+        private int _head;
+        private int _tail;
+        private int _bufferSize;
+        private int _length;
+
+        object _lock = new object();
+
+        public CircularBuffer(int bufferSize)
+        {
+            _bufferSize = bufferSize;
+            _buffer = new T[bufferSize];
+            _head = bufferSize - 1;
+        }
+
+        public bool IsEmpty
+        {
+            get { return _length == 0; }
+        }
+
+        public bool IsFull
+        {
+            get { return _length == _bufferSize; }
+        }
+
+        private int NextPosition(int position)
+        {
+            return (position + 1) % _bufferSize;
+        }
+
+        public T Dequeue()
+        {
+            lock (_lock)
+            {
+                if (IsEmpty) throw new InvalidOperationException("Buffer is empty!");
+                T dequeued = _buffer[_tail];
+                _tail = NextPosition(_tail);
+                _length--;
+                return dequeued;
+            }
+        }
+
+        public void Enqueue(T itemToAdd)
+        {
+            _head = NextPosition(_head);
+            _buffer[_head] = itemToAdd;
+            if (IsFull)
+                _tail = NextPosition(_tail);
+            else
+                _length++;
+        }
+
+        public List<T> Peek()
+        {
+            if (IsEmpty)
+                return null;
+            else
+            {
+                List<T> list = new List<T>();
+                for (int i = _tail; i <= _head; i = NextPosition(i))
+                {
+                    list.Add(_buffer[i]);
+                }
+                return list;
+            }
+        }
+    }
+}
---
+++ MyCircularBuffer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCircularBuffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CircularBuffer<int> buffer = new CircularBuffer<int>(20);
+
+            for (int i = 0; i < 10; i++)
+            {
+                buffer.Enqueue(i);
+            }
+
+            var list = buffer.Peek();
+
+            Console.WriteLine("peeking...");
+
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Console.WriteLine("dequeueing...");
+            while (!buffer.IsEmpty)
+            {
+                Console.WriteLine(buffer.Dequeue());
+            }
+        }
+    }
+}
